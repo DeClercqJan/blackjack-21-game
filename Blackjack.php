b7dac08022a12364cc079844e5450e61195d3c87
jan <declercqjan@gmail.com>
2019-12-12 11:01:08 +0100

    knopjes werken ongeveer, nu pauze., al begonnen aan het optellen van punten

--- Blackjack.php
+++ Blackjack.php
@@ -1,15 +1,25 @@
-<?php 
-echo "test Blackjack.php"; 
-class Blackjack {
+<?php
+echo "test Blackjack.php";
+// VRAAG: moet ik hier ook session_start(); toevoegen?? Wordt ingeladen in een andere pagina, lijkt me. idd, is niet nodig, want krijg anders error
+
+class Blackjack
+{
     // Add a property to this class called score. This property should have the value of the player's score at all times.
-    $score = 0;
-    function hit () {
-// Hit should add a card between 1-11.
+    // OPGELET: die property public/private/protected moet er precies bij
+    public $score;
+    function hit()
+    {
+        echo "test hit";
+        $this->score++;
+        echo $this->score;
+        // Hit should add a card between 1-11.
     }
-    function add () {
-// Stand should end your turn and start the dealer's turn. (Your point total is saved.) 
+    function add()
+    {
+        // Stand should end your turn and start the dealer's turn. (Your point total is saved.) 
     }
-    function surrender () {
-//        Surrender should make you surrender the game. (Dealer wins.)   
+    function surrender()
+    {
+        //        Surrender should make you surrender the game. (Dealer wins.)   
     }
 }
--- game.php
+++ game.php
@@ -1,10 +1,44 @@
 <?php
-echo "test game.php";
-var_dump($_GET);
+session_start();
+// echo "test game.php";
+// var_dump($_GET);
 if (isset($_GET["play-game-button"])) {
-    echo "you've succefully clicked the button";
+    // echo "you've succefully clicked the button";
+    // On game.php instantiate the Blackjack class twice, insert it into a player variable and a dealer variable
+    require("Blackjack.php");
+    $player = new Blackjack();
+    $dealer = new Blackjack();
+    $_SESSION["player"] = $player;
+    $_SESSION["dealer"] = $dealer;
+    echo $player->hit();
+    ?>
+    <form method="POST" action="game.php">
+        <input type="checkbox" id="hit" name="player-action" checked>
+        <label for="hit">hit</label>
+        <input type="checkbox" id="stand" name="player-action">
+        <label for="stand">stand</label>
+        <input type="checkbox" id="surrender" name="player-action">
+        <label for="surrender">surrender</label>
+        <input type="submit">
+    </form>
+<?php
+} else if (isset($_POST["player-action"])) {
+    require("Blackjack.php");
+    var_dump($_SESSION["player"]);
+    var_dump($_SESSION["dealer"]);
+    // Use forms to send to the game.php page what the player's action is. (i.e. hit/stand/surrender)
+
+    ?>
+    <form method="POST" action="game.php">
+        <input type="checkbox" id="hit" name="player-action" checked>
+        <label for="hit">hit</label>
+        <input type="checkbox" id="stand" name="player-action">
+        <label for="stand">stand</label>
+        <input type="checkbox" id="surrender" name="player-action">
+        <label for="surrender">surrender</label>
+        <input type="submit">
+    </form>
+<?php
 } else {
     echo "you need to go to the home page and click the button to play";
 }
-require("Blackjack.php");
-// On game.php instantiate the Blackjack class twice, insert it into a player variable and a dealer variable
